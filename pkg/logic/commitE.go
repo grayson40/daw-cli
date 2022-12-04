6cbed41e9732cf0950270cc94cb427c78eebf22a
Grayson Crozier <graysoncrozier40@gmail.com>
2022-12-03 19:06:33 -0500

    Add file tracking

--- pkg/logic/commitE.go
+++ pkg/logic/commitE.go
@@ -23,66 +23,78 @@ func newCommit(files []types.File, message string) types.Commit {
 
 // Executes the commiting process
 func ExecuteCommit(message string) {
+	// Throw error if not an initialized repo
+	if !IsInitialized() {
+		fmt.Println("fatal: not a daw repository (or any of the parent directories): .daw")
+		return
+	}
+
 	// Read staged files
-	files := ReadStaged()
+	files := GetStaged()
 
-	// Read commit stack
-	commits := ReadCommits()
-
-	// Create new commit and write to commit stack
-	commit := newCommit(files, message)
-	commits = append([]types.Commit{commit}, commits...)
-	err := writeCommit(commits)
+	trackedFiles, err := GetTracked()
 	if err != nil {
 		panic(err)
 	}
 
+	// TODO: if file not tracked, add to tracked files
+	for _, file := range files {
+		if !IsTrackedFile(file.Name) {
+			trackedFiles = append(trackedFiles, types.File{Name: file.Name, Path: file.Path})
+		}
+	}
+	writeTracked(trackedFiles)
+
+	// Read commit stack
+	commits := GetCommits()
+
+	// Create new commit and write to commit stack
+	commit := newCommit(files, message)
+	commits = append([]types.Commit{commit}, commits...)
+	writeErr := writeCommit(commits)
+	if writeErr != nil {
+		panic(writeErr)
+	}
+
 	// Clear staged files
-	if err := os.Truncate("staged.json", 0); err != nil {
+	if err := os.Truncate("./.daw/staged.json", 0); err != nil {
 		panic(err)
 	}
 }
 
 // Reads contents of json staged files and returns array of staged files
-func ReadStaged() []types.File {
+func GetStaged() []types.File {
 	var files []types.File
 
-	if _, err := os.Stat("staged.json"); err == nil {
-		jsonFile, err := os.Open("staged.json")
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		defer jsonFile.Close()
-
-		byteValue, _ := ioutil.ReadAll(jsonFile)
-		json.Unmarshal(byteValue, &files)
-
-		return files
+	jsonFile, err := os.Open("./.daw/staged.json")
+	if err != nil {
+		fmt.Println(err)
 	}
 
-	return nil
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &files)
+
+	return files
 }
 
 // Reads contents of json commit file and returns array of commits
-func ReadCommits() []types.Commit {
+func GetCommits() []types.Commit {
 	var commits []types.Commit
 
-	if _, err := os.Stat("commits.json"); err == nil {
-		jsonFile, err := os.Open("commits.json")
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		defer jsonFile.Close()
-
-		byteValue, _ := ioutil.ReadAll(jsonFile)
-		json.Unmarshal(byteValue, &commits)
-
-		return commits
+	jsonFile, err := os.Open("./.daw/commits.json")
+	if err != nil {
+		fmt.Println(err)
 	}
 
-	return nil
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &commits)
+
+	return commits
+
 }
 
 // Writes commit array to json file, returns err
@@ -92,7 +104,18 @@ func writeCommit(commits []types.Commit) error {
 		panic(err2)
 	}
 
-	err := ioutil.WriteFile("commits.json", file, 0644)
+	err := ioutil.WriteFile("./.daw/commits.json", file, 0644)
 
 	return err
+}
+
+func writeTracked(files []types.File) error {
+	file, err := json.MarshalIndent(files, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+
+	writeErr := ioutil.WriteFile("./.daw/tracked.json", file, 0644)
+
+	return writeErr
 }
