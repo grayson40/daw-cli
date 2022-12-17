97063719c63b062332b3aa667ec0af200d08451e
Grayson <graysoncrozier40@gmail.com>
2022-12-17 14:55:09 -0500

    configure flask rest api

---
+++ api/app.py
@@ -0,0 +1,68 @@
+from flask import Flask, jsonify, request
+from .db.users import *
+
+app = Flask(__name__)
+
+
+@app.route("/user", methods=["GET", "POST"])
+def user():
+    # Get user from db
+    if request.method == "GET":
+        # Parse args
+        user_email = request.args.get("email")
+        user_name = request.args.get("username")
+        user_id = request.args.get("id")
+
+        # Get user by email or username
+        if user_email != None:
+            return get_user_by_email(user_email), "200"
+        elif user_name != None:
+            return get_user_by_user_name(user_name), "200"
+        elif user_id != None:
+            return get_user_by_id(user_id), "200"
+
+    # Create user & post to db
+    elif request.method == "POST":
+        content_type = request.headers.get("Content-Type")
+        if content_type == "application/json":
+            json = request.json
+            print(json["projects"])
+            create_user(json["username"], json["email"], json["projects"])
+            return "", "204"
+        else:
+            return "Content-Type not supported!"
+
+
+# Return all users
+@app.route("/users", methods=["GET"])
+def users():
+    return get_users()
+
+# # Return all user projects
+# @app.route("/projects", methods=["GET"])
+# def projects():
+#     user_id = request.args.get("id")
+#     return get_user_projects(user_id)
+
+
+# @app.route("/channels")
+# def get_channels():
+#     return jsonify(channel_rack)
+
+
+# @app.route("/channels/instruments", methods=["GET", "POST"])
+# def instruments():
+#     if request.method == "GET":
+#         return jsonify(channel_rack["instruments"])
+#     elif request.method == "POST":
+#         channel_rack["instruments"].append(request.get_json())
+#         return "", 204
+
+
+# @app.route("/channels/samplers", methods=["GET", "POST"])
+# def samplers():
+#     if request.method == "GET":
+#         return jsonify(channel_rack["samplers"])
+#     elif request.method == "POST":
+#         channel_rack["samplers"].append(request.get_json())
+#         return "", 204
---
+++ api/db/users.py
@@ -0,0 +1,71 @@
+import pymongo
+import os
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from dotenv import load_dotenv
+
+# Creates user dictionary
+User = lambda username, email, projects: {
+    "username": username,
+    "email": email,
+    "projects": projects,
+}
+
+# Load env variables
+load_dotenv()
+
+# Connect to mongodb client
+client = pymongo.MongoClient(os.environ.get("MONGODB_URL"))
+db = client.test
+
+# Connect to db
+db = client.daw
+
+# Get users collection
+users_collection = db.users
+
+# Returns list of users in db
+def get_users():
+    # Get users
+    cursor = users_collection.find()
+    users_cursor = list(cursor)
+
+    # Converting to json
+    users_list = dumps(users_cursor, indent=4)
+
+    return users_list
+
+
+# Post user to db
+def create_user(username, email, projects):
+    user = User(username=username, email=email, projects=projects)
+    user_id = users_collection.insert_one(user).inserted_id
+    return user_id
+
+
+# Get user by email
+def get_user_by_email(email):
+    cursor = users_collection.find_one({"email": email})
+    user = dumps(cursor, indent=4)
+    return user
+
+
+# Get user by username
+def get_user_by_user_name(user_name):
+    cursor = users_collection.find_one({"username": user_name})
+    user = dumps(cursor, indent=4)
+    return user
+
+
+# Get user by id
+def get_user_by_id(user_id):
+    cursor = users_collection.find_one(ObjectId(user_id))
+    user = dumps(cursor, indent=4)
+    return user
+
+
+# Get user projects
+def get_user_projects(user_id):
+    cursor = users_collection.find_one(ObjectId(user_id))
+    user = dumps(cursor, indent=4)
+    return user["projects"]
