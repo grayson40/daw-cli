c138334c82d5f8c3aa5dee891d029ca8c3b0f0b1
Grayson <graysoncrozier40@gmail.com>
2023-01-04 12:24:00 -0500

    Move fl logic to api dir

--- pkg/scripts/mixer.py
+++
@@ -1,6 +0,0 @@
-# Copyright © 2022 Grayson Crozier <grayson40@gmail.com>
-import json
-
-# Writes project mixer to json
-def getMixer(project):
-    print(project.mixer)
--- pkg/scripts/plugins.py
+++
@@ -1,6 +0,0 @@
-# Copyright © 2022 Grayson Crozier <grayson40@gmail.com>
-import json
-
-# Writes project plugins to json
-def getPlugins(project):
-    print("project.plugins")
